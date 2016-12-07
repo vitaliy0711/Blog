77d5eb92936f134aef768850be7399e0c3133e37
vitaliy <vitaliysokha@gmail.com>
2016-12-07 17:17:06 +0200

    Connect to BD file

---
+++ functions/connect.php
@@ -0,0 +1,17 @@
+<?php
+
+$mysqli = false;
+function connectDB()
+{
+	global $mysqli;
+	$mysqli = new mysqli("127.0.0.1", "root", "", "database");
+	$mysqli->query("SET NAMES 'utf-8'");
+	
+}
+function closeDB()
+{
+	global $mysqli;
+	$mysqli->close();
+}
+
+?>
